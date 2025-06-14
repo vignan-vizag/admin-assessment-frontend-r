c642dda3fa1781b8e9f6eb4a3ebd9e3c279744c2
elitecoder05 <varma.gurijala@gmail.com>
2025-06-14 07:47:46 +0530

    aaaa

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+    plugins: [react(), tailwindcss(),],
+
+  server: {
+    host: '0.0.0.0', // This is crucial
+    port: 5173
+  }
 })
